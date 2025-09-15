dad4d1bf4a833516aab73c6ad67b77050eaabb8d
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-09-14 19:05:14 -0500

    feat: add OpenAPI configuration and security

---
+++ src/main/java/com/gym/notification_microservice/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.gym.notification_microservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+        @Bean
+        public OpenAPI customOpenAPI(
+                        @Value("${spring.application.name:Notification API}") String applicationName,
+                        @Value("${server.port:8085}") String serverPort) {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title(applicationName + " - API")
+                                                .description("REST API para gestión de notificaciones del gimnasio.")
+                                                .version("1.0.0"))
+                                .servers(List.of(
+                                                new Server().url("http://localhost:" + serverPort)
+                                                                .description("Servidor de Desarrollo Local")));
+        }
+}
